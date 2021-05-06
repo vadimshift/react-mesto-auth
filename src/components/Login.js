b84dce4e8989bc06bd189dc2207fb866c993fbea
vadimshift <vadimshift@yandex.ru>
2021-05-06 20:59:33 +0500

    верстаю страницу авторизации

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,13 +1,26 @@
 function Login() {
   return (
-    <section className="">
-      <h2 className="profile__title">Регистрация</h2>
-      <form className="" noValidate >
-        <input className="" placeholder="Email"></input>
-        <input className="" placeholder="Пароль" type="password"></input>
-        <button className="" type="button">Зарегистрироваться</button>
-        <p className="profile__title">Уже зарегистрированы? <a href="#">Войти</a></p>
-      </form>
+    <section className="auth-page">
+      <div className="auth-page__container">
+        <h2 className="auth-page__title">Регистрация</h2>
+        <form className="auth-page__form" noValidate>
+          <input className="auth-page__input" placeholder="Email"></input>
+          <input
+            className="auth-page__input"
+            placeholder="Пароль"
+            type="password"
+          ></input>
+          <button className="auth-page__submit-button" type="button">
+            Зарегистрироваться
+          </button>
+          <p className="auth-page__subtitle_type_register">
+            Уже зарегистрированы?{" "}
+            <a className="auth-page__register-link" href="#">
+              Войти
+            </a>
+          </p>
+        </form>
+      </div>
     </section>
   );
 }
