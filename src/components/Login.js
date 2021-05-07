0a17b88dcc78b2285e10d691d2abd9f0f2d61b03
Vadim Yuzmekhametov <vadimshift@yandex.ru>
2021-05-07 16:29:55 +0500

    написал стили для Login, прижал footer к низу страницы

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,26 +1,24 @@
 function Login() {
   return (
     <section className="auth-page">
-      <div className="auth-page__container">
-        <h2 className="auth-page__title">Регистрация</h2>
-        <form className="auth-page__form" noValidate>
-          <input className="auth-page__input" placeholder="Email"></input>
-          <input
-            className="auth-page__input"
-            placeholder="Пароль"
-            type="password"
-          ></input>
-          <button className="auth-page__submit-button" type="button">
-            Зарегистрироваться
-          </button>
-          <p className="auth-page__subtitle_type_register">
-            Уже зарегистрированы?{" "}
-            <a className="auth-page__register-link" href="#">
-              Войти
-            </a>
-          </p>
-        </form>
-      </div>
+      <h2 className="auth-page__title">Регистрация</h2>
+      <form className="auth-page__form" noValidate>
+        <input className="auth-page__input" placeholder="Email"></input>
+        <input
+          className="auth-page__input"
+          placeholder="Пароль"
+          type="password"
+        ></input>
+        <button className="auth-page__submit-button" type="button">
+          Зарегистрироваться
+        </button>
+        <p className="auth-page__subtitle_type_register">
+          Уже зарегистрированы?{' '}
+          <a className="auth-page__register-link" href="#">
+            Войти
+          </a>
+        </p>
+      </form>
     </section>
   );
 }
